72266f350cb88bc153f6bd73ff9f32f7c9c7a53e
Suneet Nangia <suneetnangia@gmail.com>
2024-09-10 20:14:50 +0100

    Added non-root settings location.
    Signed-off-by: Suneet Nangia <suneetnangia@gmail.com>

--- Http.Mqtt.Connector.Svc/Program.cs
+++ Http.Mqtt.Connector.Svc/Program.cs
@@ -6,5 +6,8 @@ builder.Services
 .AddConfig(builder.Configuration)
 .AddDependencies();
 
+// Add option to read settings from a different folder than root, for container mounts.
+builder.Configuration.AddJsonFile("settings/appsettings.json", optional: true);
+
 var host = builder.Build();
 host.Run();
